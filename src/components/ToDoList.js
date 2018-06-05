5a1785a3975c76e52b8c80dcd359e534d4337dcb
Ryo Utsunomiya <millionsnowliving@gmail.com>
2018-06-06 00:29:08 +0900

    Add mutation

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
-import { graphql } from 'react-apollo';
+import { graphql, compose } from 'react-apollo';
 
 const TODOS_QUERY = gql`
   query ToDosQuery {
@@ -11,20 +11,50 @@ const TODOS_QUERY = gql`
   }
 `;
 
-class ToDoList extends Component {
-  render() {
-    if (this.props.data && this.props.data.loading) {
-      return <div>Loading...</div>;
+const ADD_MUTATION = gql`
+  mutation AddMutation($input: ToDoInput) {
+    addToDo(input: $input) {
+      title
     }
+  }
+`;
 
-    const todoList = this.props.data.todos;
+class ToDoList extends Component {
+  state = { title: ' '}
+
+  render() {
+    const todoList = this.props.data.todos || [];
 
     return (
-      <div>{todoList.map(
-        todo => <p key={todo.id}>{todo.title}</p>
-      )}</div>
+      <div>
+        <input
+          value={this.state.title}
+          onChange={e => this.setState({title: e.target.value})}
+        />
+        <button type="submit" onClick={() => this._addToDo()}>
+          Add
+        </button>
+        {todoList.map(
+          todo => <p key={todo.id}>{todo.title}</p>
+        )}
+      </div>
     );
+  }
+
+  _addToDo = () => {
+    this.props.addMutation({
+      variables: {
+        input: {
+          title: this.state.title
+        }
+      },
+      refetchQueries: [{ query: TODOS_QUERY }],
+    }).then(({data}) => console.log(data.addToDo));
+    this.setState({ title: '' });
   }
 }
 
-export default graphql(TODOS_QUERY)(ToDoList);
+export default compose(
+  graphql(TODOS_QUERY),
+  graphql(ADD_MUTATION, { name: 'addMutation' })
+)(ToDoList);
